a0e4695aa74939aaf8e73451122767d1c92c393a
Adrián David Laínez Hernández <adriandavidlainezhernandez@gmail.com>
2026-04-04 18:43:51 -0600

    refactor(api): created new apps for traffic and infrastructure management, moved models and views accordingly, updated API routes and settings.

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,14 +1,5 @@
 from django.db import models
 
-class AccessLog(models.Model):
-    endpoint = models.CharField(max_length=255, help_text="Ruta consultada")
-    method = models.CharField(max_length=10)
-    ip_address = models.GenericIPAddressField(null=True, blank=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"[{self.method}] {self.endpoint} - {self.timestamp.strftime('%Y-%m-%d %H:%M:%S')}"
-
 class Usuario(models.Model):
     nombre = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'api',
+    'infraestructura',
+    'trafico',
 ]
 
 MIDDLEWARE = [
