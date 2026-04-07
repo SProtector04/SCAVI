94ed110cf095ed8014653433464f9b0c9b9abc21
Adrián David Laínez Hernández <adriandavidlainezhernandez@gmail.com>
2026-04-06 21:33:40 -0600

    fix(api): Update Usuario model to extend AbstractUser and add role field

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=100)
-    apellidos = models.CharField(max_length=100)
-    telefono = models.CharField(max_length=15)
-    fecha_registro = models.DateTimeField(auto_now_add=True)
-
+class Usuario(AbstractUser):
+    ROLES = (
+        ('ADMIN', 'Administrador'),
+        ('SUPERVISOR', 'Supervisor'),
+    )
+    rol = models.CharField(max_length=20, choices=ROLES, default='SUPERVISOR')
 
     def __str__(self):
-        return f"{self.nombre} {self.apellidos}"
+        return f"{self.username} - {self.rol}"
