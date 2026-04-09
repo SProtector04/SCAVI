92b9da7a17f04c06554011a54c41b0aad9298614
Adrián David Laínez Hernández <adriandavidlainezhernandez@gmail.com>
2026-04-09 00:10:30 -0600

    feat(anpr): register ANPR app, add media config, wire URLs
    - Add 'anpr' to INSTALLED_APPS
    - Add MEDIA_URL and MEDIA_ROOT for image storage
    - Add /api/anpr/ route in api/urls.py
    - Add media file serving in development

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'api',
     'infraestructura',
     'trafico',
+    'anpr',
 ]
 
 MIDDLEWARE = [
@@ -148,6 +149,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Media files (ANPR images)
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
